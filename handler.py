709e2d13de67ea5e591b9d2f3c670954a2ee43bd
k_nishizawa <k_nishizawa@jsl.co.jp>
2017-09-07 17:45:06 +0900

    利用APIを変更。python3未対応だった。

--- handler.py
+++ handler.py
@@ -6,11 +6,10 @@ from datetime import datetime as dt
 from connpass import get_event_connpass, get_event_connpass_id
 
 
-api = twitter.Api(consumer_key=os.getenv('CONSUMER_KEY'),
-                  consumer_secret=os.getenv('CONSUMER_SECRET'),
-                  access_token_key=os.getenv('ACCESS_TOKEN'),
-                  access_token_secret=os.getenv('ACCESS_TOKEN_SECRET'))
-
+t = twitter.Twitter(auth=twitter.OAuth(token=os.getenv('ACCESS_TOKEN'),
+                                       token_secret=os.getenv('ACCESS_TOKEN_SECRET'),
+                                       consumer_key=os.getenv('CONSUMER_KEY'),
+                                       consumer_secret=os.getenv('CONSUMER_SECRET')))
 
 def handle(event, context):
     tweet_connpass()
@@ -37,7 +36,8 @@ def tweet_connpass():
 
     for post_text in post_texts:
         # print(post_text)
-        api.PostUpdate(post_text)
+        t.statuses.update(status=post_text)
+        # t.statuses.update(status='PyCon JP 2017 始まりました！（テスポ）')
 
 
 if __name__ == '__main__':
